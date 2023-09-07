242795b9b3ef92bf10edec9d2a3114fd9cb66353
Gerald <geraldjialeizhu@gmail.com>
2023-09-07 17:06:09 -0400

    Added some more unit tests

--- pg.test.js
+++ pg.test.js
@@ -4,8 +4,22 @@ test('Is Jest Running', () => {
     expect(1).toBe(1);
 });
 
+/*
 test('Required the number of shares the investor has for ticker', () => {
     expect(getShare("Investor", "GS")).toBe(1);
 });
 
+*/
 
+
+test('Authenticates the right credentials', () => {
+    expect(authenticateUser("Investor", "Password")).toBe(true);
+});
+
+test('Rejects the wrong credentials', () => {
+    expect(authenticateUser("Not Investor", "Not Password")).toBe(false);
+});
+
+test('Retrieves the portfolio for the right credentials', () => {
+    expect(retrievePortfolio("Investor", "Password")).toBe(true);
+});
